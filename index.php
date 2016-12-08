12e15dcef99e359db3641c33c393327d4a8fe78b
Doran Pauka <doranpauka@gmail.com>
2016-12-08 15:07:24 +0100

    Testing monolog

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-echo "Hello World";
+require __DIR__ . '/vendor/autoload.php';
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+$log->addWarning('Foo!');
 ?>
