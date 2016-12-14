3e6520fdc04fc2f309e8e89d041846d2d10849db
Doran Pauka <doranpauka@gmail.com>
2016-12-14 12:27:46 +0100

    Cleared failed composer deps, installed Monolog

--- index.php
+++ index.php
@@ -3,4 +3,5 @@ require __DIR__ . '/vendor/autoload.php';
 $log = new Monolog\Logger('name');
 $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
 $log->addWarning('Foo!');
+
 ?>
